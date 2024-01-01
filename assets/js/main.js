94ec0079f3eeaa47ed69fa58790d445814fd5d64
adibmansuri511 <adibmansuri.developer@gmail.com>
2024-01-01 23:53:24 +0530

    Update files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,48 +1,46 @@
-function largestNumber() {
+// program for a simple calculator
 
-    // Take number01 from the input
-    const num01 = Number(document.getElementById("number01").value);
 
-    // Take number02 from the input
-    const num02 = Number(document.getElementById("number02").value);
+function calculator() {
 
-    // Take number03 from the input
-    const num03 = Number(document.getElementById("number03").value);
+    // Take the operator input
+    const operator = document.getElementById("operator").value;
 
-    
+    // Take the operand input
+    const firstNumber = parseInt(document.getElementById("firstNumber").value);
+    const secondNumber = parseInt(document.getElementById("secondNumber").value);
+
+
     // Check the three input numbers which is the largest one.
 
-    let result = "";
+    let result;
 
-    // Check if num01 is largest
-    if (num01 > num02 && num01 > num03) {
-
-        result = `The largest number is : ${num01}`;
-        console.log(result);
-        document.querySelector('#h4').innerHTML = result;
-
-    }
-    // Check if num02 is largest
-    else if (num02 > num01 && num02 > num03) {
-
-        result = `The largest number is : ${num02}`;
-        console.log(result);
-        document.querySelector('#h4').innerHTML = result;
-
-    }
-    // Check if num03 is largest
-    else if (num03 > num01 && num03 > num02) {
-
-        result = `The largest number is : ${num03}`;
-        console.log(result);
-        document.querySelector('#h4').innerHTML = result;
-
-    } else {
-
-        result = `All numbers are equal.`;
-        console.log(result);
-        document.querySelector('#h4').innerHTML = result;
-
+    // Using if...else if... else
+    if (operator === '+') {
+        result = firstNumber + secondNumber;
+        result = `${firstNumber} + ${secondNumber} = ${result}`;
     }
 
+    else if (operator === '-') {
+        result = firstNumber - secondNumber;
+        result = `${firstNumber} - ${secondNumber} = ${result}`;
+    }
+
+    else if (operator === '*') {
+        result = firstNumber * secondNumber;
+        result = `${firstNumber} * ${secondNumber} = ${result}`;
+    }
+
+    else if (operator === '/') {
+        result = firstNumber / secondNumber;
+        result = `${firstNumber} / ${secondNumber} = ${result}`;
+    }
+
+    else {
+        result = `Enter the valid operator.`
+    }
+
+    // Display the result
+    console.log(result);
+    document.querySelector('#h4').innerHTML = result;
 }
